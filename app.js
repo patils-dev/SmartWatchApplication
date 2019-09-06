e3875d47a2c0f7e45cf17390fed6435226884b2b
krushnap <smitakpatil26@gmail.com>
2019-09-06 17:32:35 +0530

    updated js file

--- app.js
+++ app.js
@@ -7,10 +7,17 @@
 $(document).ready(function() {
     
     $("#time-btn").click(function displaytime(){
-        
+
     });
   });
     
+  $(document).ready(function(){
+    $("#music-btn").click(function(){
+      $(".inner-rectangle").hide();
+      $(".message-top").hide();
+      $(".playMusic").show();
+    });
+}); 
 function displaytime(){
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() 
