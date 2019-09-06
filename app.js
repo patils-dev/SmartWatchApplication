cdac8d77dfaee8a9bf022574538dfff431c6bdba
krushnap <smitakpatil26@gmail.com>
2019-09-06 17:05:17 +0530

    javascript work

---
+++ app.js
@@ -0,0 +1,17 @@
+ $(document).ready(function(){
+    $("#msg-btn").click(function(){
+      $(".inner-rectangle").hide();
+      $(".message-top").show();
+    });
+}); 
+$(document).ready(function() {
+    
+    $("#time-btn").click(function displaytime(){
+        
+    });
+  });
+    
+function displaytime(){
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() 
+}
