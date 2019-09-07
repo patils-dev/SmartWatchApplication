1037cd4a232dda12564357ea4c79dd6641b5427b
krushnap <smitakpatil26@gmail.com>
2019-09-07 14:40:40 +0530

    updated Application

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  const msgArray=[{name:"Aditi",msg:"Hello. How are you today?",time:11.00},
  {name:"Shital",msg:"Hey! I'm fine. How are you??",time:4.05},
  {name:"Jotiraj",msg:"Todays weather is very cloudy.what do you gonna do today? ",time:7.00},
- {name:"Shital",msg:"Hii, where are you? Have you read today's news?? ",time:6.00}
+ {name:"Anaya",msg:"Hii, where are you? Have you read today's news?? ",time:6.00}
 ]
  
  $(document).ready(function(){
@@ -65,37 +65,110 @@ $(document).ready(function(){
       $(".full-msg").show();
     });
 }); 
-$(document).ready(function() {   
-    $("#timer-btn").click(function displaytime(){
+// $(document).ready(function() {   
+//     $("#timer-btn").click(function displaytime(){
+//         $(".timer").hide();
+//         $(".message-top").hide();
+//        $(".playMusic").hide();
+//        $(".full-msg").hide();
+//        $(".stopwatch").show();
+//        let timer=document.getElementById("display-timer");
+//        let togglebtn=document.getElementById("start-btn");
+//        let resetbtn=document.getElementById("reset-btn");
+//        let watch=new stopwatch(timer);
+//        $("#start-btn").click(function(){
+//            console.log("in fun")
+//            if(watch.isOn){
+//                watch.stop();
+//                togglebtn.textContent='start';
+//            }
+//            else{
+//             watch.start();
+//             togglebtn.textContent='stop';
+//            }
+//        })
+//        $("#reset-btn").click(function(){
+//             watch.reset();
+//         })
+//         $("$lap-btn").click(function(){
+//             watch.lap();
+//         })
+//     });
+//   });
+  
+  
+$("#timer-btn").click(function displaytime() {   
         $(".timer").hide();
         $(".message-top").hide();
        $(".playMusic").hide();
        $(".full-msg").hide();
        $(".stopwatch").show();
-       let timer=document.getElementById("display-timer");
-       let togglebtn=document.getElementById("start-btn");
-       let resetbtn=document.getElementById("reset-btn");
-       let watch=new stopwatch(timer);
-       $("#start-btn").click(function(){
-           console.log("in fun")
-           if(watch.isOn){
-               watch.stop();
-               togglebtn.textContent='start';
-           }
-           else{
-            watch.start();
-            togglebtn.textContent='stop';
-           }
-       })
-       $("#reset-btn").click(function(){
-            watch.reset();
-        })
-    });
-  });
-  
-  
-  
+// });
+// $("#start-btn").click(function displaytime() {   
+//     $(".timer").hide();
+//     $(".message-top").hide();
+//    $(".playMusic").hide();
+//    $(".full-msg").hide();
+//    $(".stopwatch").show();
+       console.log("runnin")
+       var h2 = document.getElementsByTagName('h2')[0],
+        start = document.getElementById('start-btn'),
+    // stop = document.getElementById(''),
+    clear = document.getElementById('reset-btn'),
+    seconds = 0, minutes = 0, hours = 0, t;
+    let pressed=false;
+    $("#start-btn").click(function(){
+        
+                   if(pressed){
+                    //    watch.stop();
+                    stop();
+                       start.textContent='start';
+                       console.log("first");
+                   }
+                   else{
+                    timer();
+                    
+                    console.log("second");
+                    start.textContent='stop';
+                   }
+                   $("#reset-btn").click(function(){
+                       reset();
+                   });
+               });
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
     
+    h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+  
+    t = setTimeout(add, 1000);
+    pressed=true;
+}
+
+function stop(){
+    pressed=false;
+    clearTimeout(t);
+}
+
+
+function reset(){
+    h2.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0; 
+    //timer();
+   }
+  
 $(document).ready(function(){
     $("#music-btn").click(function(){
       $(".timer").hide();
@@ -131,6 +204,9 @@ let d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, mi
 let newD = new Date();
 let n = newD.getDay();
 let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+document.getElementById("monthdisplay").innerHTML=monthNames[newD.getMonth()]+' 0'+newD.getDate();
 document.getElementById("timedisplay").innerHTML =d;
 document.getElementById("timedisplay1").innerHTML=d;
 document.getElementById("timedisplay2").innerHTML=d;
@@ -138,3 +214,4 @@ document.getElementById("timedisplay3").innerHTML=d;
 document.getElementById("daydisplay").innerHTML=week[n];
 }
 window.onload=displayData();
+});
