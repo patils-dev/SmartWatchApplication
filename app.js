a7350640e4fd47a4c454a0be37b42f0646d77227
krushnap <smitakpatil26@gmail.com>
2019-09-07 17:14:44 +0530

    updated js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const msgArray=[{name:"Aditi",msg:"Hello. How are you today?",time:11.00},
+ const msgArray=[{name:"Aditi",msg:"Hello. How are you today?",time:11.00},
  {name:"Shital",msg:"Hey! I'm fine. How are you??",time:4.05},
  {name:"Jotiraj",msg:"Todays weather is very cloudy.what do you gonna do today? ",time:7.00},
  {name:"Anaya",msg:"Hii, where are you? Have you read today's news?? ",time:6.00}
@@ -20,13 +20,10 @@ $(document).ready(function(){
       $(".playMusic").hide();
       $(".stopwatch").hide();
       $(".message-top").hide();
-        //   console.log(msgArray[0].msg1)
-        // let selectedMsg=document.getElementById("")
-        
+    
         document.getElementById("msg-name").innerHTML=msgArray[0].name;
         document.getElementById("msg").innerHTML=msgArray[0].msg;
-        // console.log(msgArray[0].time)
-        // document.getElementsByClassName("time-left[0]").innerHTML=msgArray[0].time;
+        
       $(".full-msg").show();
     });
 }); 
@@ -65,7 +62,16 @@ $(document).ready(function(){
       $(".full-msg").show();
     });
 }); 
-
+$(document).ready(function() {   
+    $("#music-btn").click(function(){
+        $(".timer").hide();
+        $(".message-top").hide();
+       $(".full-msg").hide();
+       $(".stopwatch").hide();
+       $(".playMusic").show();
+    });
+       
+  
   
 $("#timer-btn").click(function displaytime() {   
         $(".timer").hide();
@@ -74,7 +80,7 @@ $("#timer-btn").click(function displaytime() {
        $(".full-msg").hide();
        $(".stopwatch").show();
 
-    //    console.log("runnin")
+       console.log("runnin")
        var h2 = document.getElementsByTagName('h2')[0],
         start = document.getElementById('start-btn'),
     // stop = document.getElementById(''),
@@ -119,8 +125,10 @@ $("#timer-btn").click(function displaytime() {
                 minutes = 0;
                 hours++;
             }
-        }      
+        }
+        
         h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
         timer();
     }
     function timer() {
@@ -128,6 +136,7 @@ $("#timer-btn").click(function displaytime() {
         t = setTimeout(add, 1000);
         pressed=true;
     }
+
     function stop(){
         pressed=false;
         clearTimeout(t);
@@ -150,7 +159,7 @@ $("#timer-btn").click(function displaytime() {
     lapcount+=1;
     
    }
-  
+})
 $(document).ready(function(){
     $("#music-btn").click(function(){
       $(".timer").hide();
