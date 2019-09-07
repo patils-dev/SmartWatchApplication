77fc3bf0bf72b8a4717f6bba74a5a223f4ecce1c
krushnap <smitakpatil26@gmail.com>
2019-09-07 08:15:38 +0530

    updated js file

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+ var d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
+var newD = new Date();
+var n = newD.getDay();
+let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+document.getElementById("timedisplay").innerHTML =d;
+document.getElementById("monthdisplay").innerHTML=week[n];
+ 
+ 
+ const msgArray=[{msg:"Hello. How are you today?",time:11.00},
+ {msg:"Hey! I'm fine. Thanks for asking!",time:4.05},
+ {msg:"Sweet! So, what do you wanna do today?",time:7.00},
+ {msg:"Nah, I dunno. Play soccer.. or learn more coding perhaps?",time:6.00}
+]
+ 
  $(document).ready(function(){
     $("#msg-btn").click(function(){
       $(".timer").hide();
       $(".playMusic").hide();
       $(".stopwatch").hide();
+      $(".full-msg").hide();
       $(".message-top").show();
     });
 }); 
@@ -12,6 +27,43 @@ $(document).ready(function(){
       $(".playMusic").hide();
       $(".stopwatch").hide();
       $(".message-top").hide();
+      console.log(msgArray[0].msg1)
+        document.getElementById("msg").innerHTML=msgArray[0].msg;
+        // console.log(msgArray[0].time)
+        // document.getElementsByClassName("time-left[0]").innerHTML=msgArray[0].time;
+      $(".full-msg").show();
+    });
+}); 
+$(document).ready(function(){
+    $("#msg2").click(function(){
+      $(".timer").hide();
+      $(".playMusic").hide();
+      $(".stopwatch").hide();
+      $(".message-top").hide();
+      console.log(msgArray[0].msg1)
+        document.getElementById("msg").innerHTML=msgArray[1].msg;
+      $(".full-msg").show();
+    });
+}); 
+$(document).ready(function(){
+    $("#msg3").click(function(){
+      $(".timer").hide();
+      $(".playMusic").hide();
+      $(".stopwatch").hide();
+      $(".message-top").hide();
+      console.log(msgArray[0].msg1)
+        document.getElementById("msg").innerHTML=msgArray[2].msg;
+      $(".full-msg").show();
+    });
+}); 
+$(document).ready(function(){
+    $("#msg4").click(function(){
+      $(".timer").hide();
+      $(".playMusic").hide();
+      $(".stopwatch").hide();
+      $(".message-top").hide();
+      console.log(msgArray[0].msg1)
+        document.getElementById("msg").innerHTML=msgArray[3].msg;
       $(".full-msg").show();
     });
 }); 
@@ -21,16 +73,50 @@ $(document).ready(function() {
         $(".timer").hide();
         $(".message-top").hide();
        $(".playMusic").hide();
+       $(".full-msg").hide();
        $(".stopwatch").show();
+       let timer=document.getElementById("display-timer");
+       let togglebtn=document.getElementById("start-btn");
+       let resetbtn=document.getElementById("reset-btn");
+       let watch=new stopwatch(timer);
+       $("#start-btn").click(function(){
+           console.log("in fun")
+           if(watch.isOn){
+               watch.stop();
+               togglebtn.textContent='start';
+           }
+           else{
+            watch.start();
+            togglebtn.textContent='stop';
+           }
+       })
+       $("#reset-btn").click(function(){
+            watch.reset();
+        })
     });
   });
+  
+  
+  
     
 $(document).ready(function(){
     $("#music-btn").click(function(){
       $(".timer").hide();
       $(".message-top").hide();
       $(".stopwatch").hide();
+      $(".full-msg").hide();
       $(".playMusic").show();
+    });
+}); 
+
+$(document).ready(function(){
+    $("#backbutton").click(function(){
+    
+      $(".message-top").hide();
+      $(".stopwatch").hide();
+      $(".full-msg").hide();
+      $(".playMusic").hide();
+      $(".timer").show();
     });
 }); 
 
@@ -38,13 +124,10 @@ $(document).ready(function(){
     $("#time-btn").click(function(){
         $(".message-top").hide();
         $(".playMusic").hide();
+        $(".full-msg").hide();
+        $(".stopwatch").hide();
         $(".timer").show();
     });
 }); 
 
-var d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
-var newD = new Date();
-var n = newD.getDay();
-let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-document.getElementById("timedisplay").innerHTML =d;
-document.getElementById("monthdisplay").innerHTML =week[n];
+console.log("app")
