22d2974dc49aeb1d6b23332640622117d3af6de7
krushnap <smitakpatil26@gmail.com>
2019-10-03 13:49:23 +0530

    changes done

--- app.js
+++ app.js
@@ -1,94 +1,74 @@
 
+
  const msgArray=[{name:"Aditi",msg:"Hello. How are you today?",time:11.00},
  {name:"Shital",msg:"Hey! I'm fine. How are you??",time:4.05},
  {name:"Jotiraj",msg:"Todays weather is very cloudy.what do you gonna do today? ",time:7.00},
  {name:"Anaya",msg:"Hii, where are you? Have you read today's news?? ",time:6.00}
 ]
- 
- $(document).ready(function(){
+
+
+$(document).ready(function(){
+
     $("#msg-btn").click(function(){
-      $(".timer").hide();
-      $(".playMusic").hide();
-      $(".stopwatch").hide();
-      $(".full-msg").hide();
+      $(".timer,.playMusic,.stopwatch,.full-msg").hide();
       $(".message-top").show();
     });
-}); 
-$(document).ready(function(){
+
+
     $("#msg1").click(function(){
-      $(".timer").hide();
-      $(".playMusic").hide();
-      $(".stopwatch").hide();
-      $(".message-top").hide();
-    
+      $(".timer,.playMusic,.stopwatch,.message-top").hide();
         document.getElementById("msg-name").innerHTML=msgArray[0].name;
         document.getElementById("msg").innerHTML=msgArray[0].msg;
-        
+
       $(".full-msg").show();
     });
-}); 
-$(document).ready(function(){
+
+
     $("#msg2").click(function(){
-      $(".timer").hide();
-      $(".playMusic").hide();
-      $(".stopwatch").hide();
-      $(".message-top").hide();
-     
+      $(".timer,.playMusic,.stopwatch,..message-top").hide();
+
       document.getElementById("msg-name").innerHTML=msgArray[1].name;
         document.getElementById("msg").innerHTML=msgArray[1].msg;
       $(".full-msg").show();
     });
-}); 
-$(document).ready(function(){
+  }); 
+
     $("#msg3").click(function(){
-      $(".timer").hide();
-      $(".playMusic").hide();
-      $(".stopwatch").hide();
-      $(".message-top").hide();
+      $(".timer,.playMusic,.stopwatch,.message-top").hide();
     
       document.getElementById("msg-name").innerHTML=msgArray[2].name;
         document.getElementById("msg").innerHTML=msgArray[2].msg;
       $(".full-msg").show();
     });
-}); 
-$(document).ready(function(){
+
+
     $("#msg4").click(function(){
-      $(".timer").hide();
-      $(".playMusic").hide();
-      $(".stopwatch").hide();
-      $(".message-top").hide();
+      $(".timer,.playMusic,.stopwatch,.message-top").hide();
       document.getElementById("msg-name").innerHTML=msgArray[3].name;
         document.getElementById("msg").innerHTML=msgArray[3].msg;
       $(".full-msg").show();
     });
-}); 
-$(document).ready(function() {   
+
+   
     $("#music-btn").click(function(){
-        $(".timer").hide();
-        $(".message-top").hide();
-       $(".full-msg").hide();
-       $(".stopwatch").hide();
+        $(".timer,.message-top,.full-msg,.stopwatch").hide();
        $(".playMusic").show();
     });
-       
-  
-  
-$("#timer-btn").click(function displaytime() {   
-        $(".timer").hide();
-        $(".message-top").hide();
-       $(".playMusic").hide();
-       $(".full-msg").hide();
+
+
+$("#time-btn").click(function displaytime() {
+        $(".timer,.message-top,.playMusic,.full-msg").hide();
        $(".stopwatch").show();
 
        console.log("runnin")
        var h2 = document.getElementsByTagName('h2')[0],
         start = document.getElementById('start-btn'),
-    // stop = document.getElementById(''),
-    clear = document.getElementById('reset-btn'),
-    seconds = 0, minutes = 0, hours = 0, t;
-    let pressed=false;
-    let lapcount=1;
-    $("#start-btn").click(function(){
+      // stop = document.getElementById(''),
+      clear = document.getElementById('reset-btn'),
+      seconds = 0, minutes = 0, hours = 0, t;
+      let pressed=false;
+      let lapcount=1;
+      $("#start-btn").click(function(){
         
                    if(pressed){
                     //    watch.stop();
@@ -97,7 +77,7 @@ $("#timer-btn").click(function displaytime() {
                     //    console.log("first");
                    }
                    else{
-                    timer();                   
+                    timer();
                     // console.log("second");
                     start.textContent='stop';
                    }
@@ -105,11 +85,8 @@ $("#timer-btn").click(function displaytime() {
                        reset();
                    });
                    $("#lap-btn").click(function(){
-                    $(".timer").hide();
-                    $(".message-top").hide();
-                    $(".stopwatch").hide();
-                    $(".full-msg").hide();
-                    $(".playMusic").hide();
+                    $(".timer,.message-top,.stopwatch,.full-msg,.playMusic").hide();
+                    
                     $(".stopwatch").show();
                        lap();
                        $(".lapsShows").show();
@@ -151,58 +128,51 @@ $("#timer-btn").click(function displaytime() {
    }
    function lap()
    {
-      
-    let laplbltxt = document.createTextNode("lap"+lapcount+" - "+hours+":"+minutes+":"+seconds);
-    
-   let lap = document.getElementById("lap-display");
-    lap.appendChild(laplbltxt);
+    let node = document.createElement("LI");
+    let textnode = document.createTextNode("LAP "+lapcount+" - "+hours+":"+minutes+":"+seconds);
+    node.appendChild(textnode);
+    document.getElementById("lap-display").appendChild(node);
     lapcount+=1;
     
    }
 })
-$(document).ready(function(){
+
     $("#music-btn").click(function(){
-      $(".timer").hide();
-      $(".message-top").hide();
-      $(".stopwatch").hide();
-      $(".full-msg").hide();
+      $(".timer,.message-top,.stopwatch,.full-msg").hide();
       $(".playMusic").show();
     });
-}); 
+ 
 
-$(document).ready(function(){
-    $("#backbutton").click(function(){
-    
-      $(".message-top").hide();
-      $(".stopwatch").hide();
-      $(".full-msg").hide();
-      $(".playMusic").hide();
+
+    $("#backbutton1,#backbutton3").click(function(){
+      $(".message-top,.stopwatch,.full-msg,.playMusic").hide();
       $(".timer").show();
+      stop();
     });
-}); 
+    $('#backbutton2').click(function(){
+      $(".timer,.playMusic,.stopwatch,.full-msg").hide();
+      $(".message-top").show();
+    })
 
-$(document).ready(function(){
     $("#time-btn").click(function(){
-        $(".message-top").hide();
-        $(".playMusic").hide();
-        $(".full-msg").hide();
-        $(".stopwatch").hide();
-        $(".timer").show();
+        $(".message-top,.playMusic,.full-msg,.timer,.time").hide();
+        $(".time").show();
+        $(".stopwatch").show();
     });
-}); 
-function displayData(){
-let d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
-let newD = new Date();
-let n = newD.getDay();
-let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-document.getElementById("monthdisplay").innerHTML=monthNames[newD.getMonth()]+' 0'+newD.getDate();
-document.getElementById("timedisplay").innerHTML =d;
-document.getElementById("timedisplay1").innerHTML=d;
-document.getElementById("timedisplay2").innerHTML=d;
-document.getElementById("timedisplay3").innerHTML=d;
-document.getElementById("daydisplay").innerHTML=week[n];
-}
-window.onload=displayData();
-});
+  
+  function displayData(){
+  let d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
+  let newD = new Date();
+  let n = newD.getDay();
+  let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  document.getElementById("monthdisplay").innerHTML=monthNames[newD.getMonth()]+' 0'+newD.getDate();
+  document.getElementById("timedisplay").innerHTML =d;
+  document.getElementById("timedisplay1").innerHTML=d;
+  document.getElementById("timedisplay2").innerHTML=d;
+  document.getElementById("timedisplay3").innerHTML=d;
+  document.getElementById("daydisplay").innerHTML=week[n];
+  }
+  window.onload=displayData();
+  
